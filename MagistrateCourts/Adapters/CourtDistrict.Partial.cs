79024aa40c7fcec0937d8ea2056f0be3b161b00e
AnAndrey <an.andrey.g@gmail.com>
2016-12-17 20:34:35 +0300

    Some files that MS VS didn't commit at the first time.

---
+++ MagistrateCourts/Adapters/CourtDistrict.Partial.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedInterfaces;
+namespace MagistrateCourts
+{
+    public partial class CourtDistrict : IChangeableData
+    {
+        public CourtDistrict(string name, string webSite)
+        {
+            Name = name;
+            WebSite = webSite;
+        }
+        public IEnumerable<IChangeableData> Childs
+        {
+            get
+            {
+                return CourtLocations;
+            }
+
+            set
+            {
+                CourtLocations = value.Cast<CourtLocation>().ToList();
+            }
+        }
+
+        public bool HasChilds
+        {
+            get
+            {
+                return CourtLocations == null ? false : CourtLocations.Any();
+            }
+        }
+
+        public string Value
+        {
+            get
+            {
+                return WebSite;
+            }
+        }
+    }
+}
---
+++ MagistrateCourts/Adapters/CourtRegion.Partial.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedInterfaces;
+
+namespace MagistrateCourts
+{
+    public partial class CourtRegion : IChangeableData
+    {
+        public CourtRegion(string name, string number)
+        {
+            Name = name;
+            Number = number;
+        }
+        public IEnumerable<IChangeableData> Childs
+        {
+            get
+            {
+                return this.CourtDistricts;
+            }
+
+            set
+            {
+#warning DOUBLE casting!!!
+                CourtDistricts = value.Cast<CourtDistrict>().ToList() ;
+            }
+        }
+
+        public bool HasChilds
+        {
+            get
+            {
+                return CourtDistricts == null? false : CourtDistricts.Any();
+            }
+        }
+
+        public string Value
+        {
+            get
+            {
+                return Number;
+            }
+        }
+    }
+}
